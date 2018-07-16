6b3cd1ae6744dc2ac3193f06dab09dd1fdc63799
michalper <michal.per@encja.com>
2018-07-16 12:00:09 +0200

    IPRESSO-6385 #comment tests part 1

---
+++ tests/AgreementTest.php
@@ -0,0 +1,175 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class AgreementTest extends TestCase
+{
+    public static $config = [
+        'url' => 'https://panel.local-ipresso.encja.eu',
+        'login' => 'api',
+        'password' => 'api.API.123',
+        'customerKey' => 'c4ca4238a0b923820dcc509a6f75849b',
+        'token' => 'd9916ea09b85e8ad111adb602edf99d7',
+    ];
+
+    /**
+     * @var iPresso
+     */
+    private $class;
+
+    /**
+     * AgreementTest constructor.
+     * @param string|null $name
+     * @param array $data
+     * @param string $dataName
+     * @throws Exception
+     */
+    public function __construct(string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->class = (new iPresso())
+            ->setLogin(self::$config['login'])
+            ->setPassword(self::$config['password'])
+            ->setCustomerKey(self::$config['customerKey'])
+            ->setToken(self::$config['token'])
+            ->setUrl(self::$config['url']);
+    }
+
+    public function testAgreementClass()
+    {
+        $this->assertInstanceOf(\iPresso\Service\AgreementService::class, $this->class->agreement);
+    }
+
+    /**
+     * @depends testAgreementClass
+     * @see http://apidoc.ipresso.pl/v2/en/#get-all-available-agreements
+     */
+    public function testAgreementGetAll()
+    {
+        $response = $this->class->agreement->get();
+
+        $this->assertInstanceOf(\iPresso\Service\Response::class, $response);
+
+        $this->assertEquals(\iPresso\Service\Response::STATUS_OK, $response->getCode());
+
+        $this->assertObjectHasAttribute('agreement', $response->getData());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testAgreementAddWrong()
+    {
+        $agreement = new \iPresso\Model\Agreement();
+
+        $this->expectException(Exception::class);
+        $agreement->getAgreement();
+    }
+
+    /**
+     * @return integer
+     * @throws Exception
+     */
+    public function testAgreementAdd()
+    {
+        $agreement = new \iPresso\Model\Agreement();
+        $agreement->setName('Unit tests');
+        $agreement->setDescription('Unit tests description.');
+        $agreement->setDmStatus(\iPresso\Model\Agreement::DIRECT_MARKETING_VISIBLE);
+        $response = $this->class->agreement->add($agreement);
+
+        $this->assertInstanceOf(\iPresso\Service\Response::class, $response);
+
+        $this->assertContains($response->getCode(), [\iPresso\Service\Response::STATUS_CREATED, \iPresso\Service\Response::STATUS_FOUND]);
+
+        $this->assertObjectHasAttribute('agreement', $response->getData());
+
+        $this->assertGreaterThan(0, $response->getData()->agreement->id);
+
+        return (integer)$response->getData()->agreement->id;
+    }
+
+    /**
+     * @depends testAgreementAdd
+     * @param integer $idAgreement
+     * @return integer
+     * @throws Exception
+     */
+    public function testAgreementEdit($idAgreement)
+    {
+        $this->assertGreaterThan(0, $idAgreement);
+
+        $agreement = new \iPresso\Model\Agreement();
+        $agreement->setName('Unit tests edition');
+        $agreement->setDescription('Unit tests edition.');
+
+        $response = $this->class->agreement->edit($idAgreement, $agreement);
+
+        $this->assertInstanceOf(\iPresso\Service\Response::class, $response);
+
+        $this->assertContains($response->getCode(), [\iPresso\Service\Response::STATUS_OK]);
+
+        return $idAgreement;
+    }
+
+
+    /**
+     * @return integer
+     * @throws Exception
+     */
+    public function testContactAdd()
+    {
+        $contact = new \iPresso\Model\Contact();
+        $contact->setEmail('michal.per+test@encja.com');
+
+        $response = $this->class->contact->add($contact);
+
+        $this->assertInstanceOf(\iPresso\Service\Response::class, $response);
+
+        $this->assertContains($response->getCode(), [\iPresso\Service\Response::STATUS_OK]);
+
+        $this->assertObjectHasAttribute('contact', $response->getData());
+
+        $contact = reset($response->getData()->contact);
+
+        $this->assertContains($contact->code, [\iPresso\Service\Response::STATUS_OK, \iPresso\Service\Response::STATUS_FOUND, \iPresso\Service\Response::STATUS_SEE_OTHER]);
+
+        $this->assertGreaterThan(0, $contact->id);
+
+        return (integer)$contact->id;
+    }
+
+    /**
+     * @depends testAgreementAdd
+     * @depends testContactAdd
+     * @param $idAgreement
+     * @param $idContact
+     */
+    public function testAddContactToAgreements(int $idAgreement, int $idContact)
+    {
+        $this->assertGreaterThan(0, $idAgreement);
+        $this->assertGreaterThan(0, $idContact);
+
+        /**
+         * @TODO
+         */
+    }
+
+
+    /**
+     * @param $idAgreement
+     * @depends testAgreementEdit
+     */
+    public function testAgreementDelete($idAgreement)
+    {
+        $this->assertGreaterThan(0, $idAgreement);
+
+        $response = $this->class->agreement->delete($idAgreement);
+
+        $this->assertInstanceOf(\iPresso\Service\Response::class, $response);
+
+        $this->assertContains($response->getCode(), [\iPresso\Service\Response::STATUS_OK]);
+    }
+
+
+}
---
+++ tests/ServiceTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ServiceTest extends TestCase
+{
+    public static $config = [
+        'url' => 'https://panel.local-ipresso.encja.eu',
+        'login' => 'api',
+        'password' => 'api.API.123',
+        'customerKey' => 'c4ca4238a0b923820dcc509a6f75849b',
+        'token' => 'd9916ea09b85e8ad111adb602edf99d7',
+    ];
+
+    /**
+     * @var iPresso
+     */
+    private $class;
+
+    public function __construct(string $name = null, array $data = [], string $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->class = (new iPresso())
+            ->setLogin(self::$config['login'])
+            ->setPassword(self::$config['password'])
+            ->setCustomerKey(self::$config['customerKey'])
+            ->setUrl(self::$config['url']);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testSetWrongUrl()
+    {
+        $this->expectException(Exception::class);
+        $class = new iPresso();
+        $class->setUrl('http://panel.ipresso.dev');
+    }
+
+    /**
+     * @depends testSetWrongUrl
+     * @throws Exception
+     */
+    public function testGetToken()
+    {
+        $token = $this->class->getToken();
+
+        $this->assertInstanceOf(\iPresso\Service\Response::class, $token);
+
+        $this->assertEquals(self::$config['token'], $token->getData());
+    }
+
+}
